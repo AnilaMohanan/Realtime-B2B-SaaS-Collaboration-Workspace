f335934e6c7fd3648bcca90c742011f167d84ef6
anilamohanan <anilamohanan89@gmail.com>
2026-07-21 21:58:41 +0530

    Add WorkspaceMember APIs

--- Server/server.ts
+++ Server/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+
 import workspaceRoutes from "./routes/workspace.routes";
 import channelRoutes from "./routes/channel.routes";
-import cors from "cors";
+import workspaceMemberRoutes from "./routes/workspaceMember.routes";
+
 
 const app = express();
 
@@ -19,9 +22,12 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/realtime_workspace")
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
+  
 
 app.use("/api/workspaces", workspaceRoutes);
 app.use("/api/channels", channelRoutes);
+app.use("/api/workspace-members", workspaceMemberRoutes);
+
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
